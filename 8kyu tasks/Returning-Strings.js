a3db542e6dc8af2860786cc37f6eb0cf32330ffe
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-03-29 01:42:26 +0200

    first commit

---
+++ 8kyu tasks/Returning-Strings.js
@@ -0,0 +1,16 @@
+/*
+  Title:
+    Returning Strings
+
+  Description:
+    Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+  Examples:
+
+  Kata Link:
+    https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript/640f5458f4a0b7a83f299c3e
+*/
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
