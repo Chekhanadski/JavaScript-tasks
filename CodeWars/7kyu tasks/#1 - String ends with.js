d306f4f6f91f09592a349d0755f252373bbd5cd1
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-03-29 22:20:37 +0200

    solvint 2 tasks

---
+++ CodeWars/7kyu tasks/#1 - String ends with.js
@@ -0,0 +1,22 @@
+/*
+Title:
+    String ends with?
+
+  Description:
+    Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+  Examples:
+    solution('abc', 'bc') // returns true
+    solution('abc', 'd') // returns false
+
+  Kata Link:
+    https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+*/
+
+function solution(str, ending){
+  let endingLenght = ending.length;
+  let strEnd = str.slice(-endingLenght);
+  
+  if ( (strEnd === ending) || (ending.length === 0) ) return true;
+  return false;
+}
