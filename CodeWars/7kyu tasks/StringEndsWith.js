8b3af94655ded03d194cde127149a7a5aaadd8c2
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-26 23:14:10 +0200

    added task "Remove the minimum" kata 7

--- CodeWars/7kyu tasks/StringEndsWith.js
+++ CodeWars/7kyu tasks/StringEndsWith.js
@@ -2,15 +2,15 @@
 Title:
     String ends with?
 
-  Description:
-    Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+Description:
+  Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
-  Examples:
-    solution('abc', 'bc') // returns true
-    solution('abc', 'd') // returns false
+Examples:
+  solution('abc', 'bc') // returns true
+  solution('abc', 'd') // returns false
 
-  Kata Link:
-    https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+Kata Link:
+  https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
 */
 
 function solution(str, ending) {
