c294aa971a2844181c86077af59978a173a2c6e5
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-27 00:01:00 +0200

    added task "Exes and Ohs" kata 7

---
+++ CodeWars/7kyu tasks/exesAndOhs.js
@@ -0,0 +1,27 @@
+/*
+Title:
+  Exes and Ohs
+
+Description:
+  Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+  XO("ooxx") => true
+  XO("xooxx") => false
+  XO("ooxXm") => true
+  XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+  XO("zzoo") => false
+
+Kata Link:
+  https://www.codewars.com/kata/55908aad6620c066bc00002a/train/javascript
+*/
+
+function XO(str) {
+  let sum = 0;
+  for (i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() == "x") sum++;
+    if (str[i].toLowerCase() == "o") sum--;
+  }
+  return sum == 0;
+}
