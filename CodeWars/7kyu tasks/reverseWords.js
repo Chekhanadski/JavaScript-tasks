287907a54c71d8d3037d8447f8654c4c85be5eb6
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-26 23:53:54 +0200

    added task "Reverse words" kata 7

---
+++ CodeWars/7kyu tasks/reverseWords.js
@@ -0,0 +1,23 @@
+/*
+Title:
+  Reverse words
+
+Description:
+  Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples:
+  "This is an example!" ==> "sihT si na !elpmaxe"
+  "double  spaces"      ==> "elbuod  secaps"
+
+Kata Link:
+  https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+*/
+
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map(function (word) {
+      return word.split("").reverse().join("");
+    })
+    .join(" ");
+}
