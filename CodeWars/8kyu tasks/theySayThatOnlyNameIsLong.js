107a426858691951a2ccf27145d4096c0714bbb7
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-28 01:12:31 +0200

    solved task "They say that only the name..." 8kyu

---
+++ CodeWars/8kyu tasks/theySayThatOnlyNameIsLong.js
@@ -0,0 +1,34 @@
+/*
+  Title:
+    They say that only the name is long enough to attract attention. They also said that only a simple Kata will have someone to solve it. This is a sadly story #1: Are they opposite?
+
+  Description:
+    Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
+
+    The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are empty then you should return false/False.
+  
+  Examples:
+    "ab","AB"     -> true
+    "aB","Ab"     -> true
+    "aBcd","AbCD" -> true
+    "AB","Ab"     -> false
+    "",""         -> false
+
+  Kata Link:
+    https://www.codewars.com/kata/574b1916a3ebd6e4fa0012e7/train/javascript
+*/
+
+function isOpposite(s1, s2) {
+  let newS1 = s1
+    .split("")
+    .map((elem, index) => {
+      if (elem === elem.toUpperCase()) {
+        return elem.toLowerCase();
+      }
+      return elem.toUpperCase();
+    })
+    .join("");
+
+  if (newS1 === "") return false;
+  return newS1 === s2;
+}
