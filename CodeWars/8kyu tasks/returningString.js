222fa05cffa723e641329035db793c723eb46270
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-27 21:36:18 +0200

    added task "Returning Strings" 8kyu

---
+++ CodeWars/8kyu tasks/returningString.js
@@ -0,0 +1,16 @@
+/*
+  Title:
+    Returning Strings
+
+  Description:    
+    Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+  [Make sure you type the exact thing I wrote or the program may not execute properly]
+  
+  Kata Link:
+    https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+*/
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
