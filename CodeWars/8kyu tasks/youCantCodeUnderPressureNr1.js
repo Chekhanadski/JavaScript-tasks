b8b4209a4971b3e566192b5ce0c103944d168a5c
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-27 20:17:28 +0200

    added task "You Can't Code Under ..." kata8

---
+++ CodeWars/8kyu tasks/youCantCodeUnderPressureNr1.js
@@ -0,0 +1,15 @@
+/*
+  Title:
+    You Can't Code Under Pressure #1
+
+  Description:
+    Code as fast as you can! You need to double the integer and return it.
+
+  Kata Link:
+    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+*/
+
+function doubleInteger(i) {
+  i = i * 2;
+  return i;
+}
