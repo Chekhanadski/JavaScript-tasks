42f207fe730f9f03621679439ae342099b9db36f
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-27 20:14:04 +0200

    added task "Grasshopper - Summation" kata 8

---
+++ CodeWars/8kyu tasks/grasshopper-Summation.js
@@ -0,0 +1,23 @@
+/*
+  Title:
+    Grasshopper - Summation
+
+  Description:
+    Summation
+      Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+  
+  Examples:
+    2 -> 3 (1 + 2)
+    8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+  Kata Link:
+    https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+*/
+
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
