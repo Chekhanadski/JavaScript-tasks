81a9cba3944f0adfab5b686ccf2242297bb13207
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-27 21:03:13 +0200

    added task "MakeUpperCase" 8kyu

---
+++ CodeWars/8kyu tasks/makeUpperCase.js
@@ -0,0 +1,14 @@
+/*
+  Title:
+    MakeUpperCase
+
+  Description:
+    Write a function which converts the input string to uppercase.
+
+  Kata Link:
+    https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
