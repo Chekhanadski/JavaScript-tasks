beec488f4dd7bb3ba58a2b086472b4b264f31bb4
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-06-28 17:35:16 +0200

    solved task "Convert boolean values..." 8kyu

---
+++ CodeWars/8kyu tasks/convertBooleanValuesToStringsYesOrNo.js
@@ -0,0 +1,17 @@
+/*
+  Title:
+    Convert boolean values to strings 'Yes' or 'No'.
+
+  Description:
+    Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+ 
+  Kata Link:
+    https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+*/
+
+function boolToWord(bool) {
+  if (bool === true) {
+    return "Yes";
+  }
+  return "No";
+}
