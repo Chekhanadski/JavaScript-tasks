fa3859e6a08ae977e352144d02ab1c00e38321e7
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-07-05 14:05:16 +0200

    solved task "Find the odd int" 6kyu

---
+++ CodeWars/6kyu tasks/findTheOddInt.js
@@ -0,0 +1,33 @@
+/*
+Title:
+  Find the odd int.
+
+Description:
+  Given an array of integers, find the one that appears an odd number of times.
+
+  There will always be only one integer that appears an odd number of times.
+
+Examples
+  [7] should return 7, because it occurs 1 time (which is odd).
+  [0] should return 0, because it occurs 1 time (which is odd).
+  [1,1,2] should return 2, because it occurs 1 time (which is odd).
+  [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+  [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+  Kata Link:
+    https://www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
+*/
+
+function findOdd(A) {
+  let obj = {};
+  let number;
+
+  let arr = A.forEach((elem) => {
+    obj[elem] = obj[elem] ? obj[elem] + 1 : 1;
+  });
+  let arr2 = Object.entries(obj);
+  arr2.map(([key, value]) => {
+    if (value % 2 !== 0) number = key;
+  });
+  return +number;
+}
