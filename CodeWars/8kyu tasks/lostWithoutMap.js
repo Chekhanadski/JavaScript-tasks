ca690d8885af8ea650c3a715f3cf99836e539b4d
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-07-27 17:52:18 +0200

    solved task "Beginner - Lost Without a Map" 8kyu

---
+++ CodeWars/8kyu tasks/lostWithoutMap.js
@@ -0,0 +1,18 @@
+/*
+  Title:
+    Beginner - Lost Without a Map
+
+  Description:
+    Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+  Kata Link:
+    https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+*/
+
+function maps(x) {
+  return x.map((elem) => elem * 2);
+}
