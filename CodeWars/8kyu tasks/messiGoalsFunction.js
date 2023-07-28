242d7e3eaef27afc4eccda3e14ac536bdda19b91
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-07-28 22:19:04 +0200

    solved task "Messi goals function" 8kuy

---
+++ CodeWars/8kyu tasks/messiGoalsFunction.js
@@ -0,0 +1,25 @@
+/*
+  Title:
+    Grasshopper - Messi goals function
+
+  Description:
+    Messi is a soccer player with goals in three leagues:
+
+      - LaLiga
+      - Copa del Rey
+      - Champions
+
+    Complete the function to return his total number of goals in all three leagues.
+
+  Note: the input will always be valid.
+
+  For example:
+
+      5, 10, 2  -->  17
+  Kata Link:
+    https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+*/
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
