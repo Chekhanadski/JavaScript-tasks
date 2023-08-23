34f4ed612c0c04d86b0b891e6779138fdebdd7a5
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-08-23 05:28:14 +0200

    solved task "Training JS #37" 7kyu

---
+++ CodeWars/7kyu tasks/trainingJS37.js
@@ -0,0 +1,32 @@
+/*
+  Title:
+    Training JS #37: Unlock new weapon---RegExp Object
+
+  Description:
+    Task
+      Coding in function countAnimals. function accept two parameters: animals, a string contains some animals; count, an array list of which animals we should count. The result should be a number array.
+
+    Examples
+      countAnimals("dog,cat",["dog","cat"]); //=> [1,1]
+      countAnimals("dog,cat",["dog","cat","pig"]); //=> [1,1,0]
+      countAnimals("dog,dog,cat",["dog","cat"]); //=> [2,1]
+      countAnimals("dog,dog,cat",["pig","cow"]); //=> [0,0]
+
+  Kata Link:
+    https://www.codewars.com/kata/5735e39313c205fe39001173/train/javascript
+*/
+
+function countAnimals(animals, count) {
+  return count.map((a) => (animals.match(new RegExp(a, "g")) || []).length);
+}
+
+///////////////////////////////////////////////////
+
+function countAnimals(animals, count) {
+  const animalArr = animals.split(",");
+  return count.reduce((acc, animal) => {
+    const animalCount = animalArr.filter((a) => a === animal).length;
+    acc.push(animalCount);
+    return acc;
+  }, []);
+}
