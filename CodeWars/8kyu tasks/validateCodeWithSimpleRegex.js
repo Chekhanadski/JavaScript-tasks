b43428c5fce5f2966f1f0a5fd070a9f42dac409b
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-08-31 18:17:46 +0200

    solved task "validate code with simple regex" 8kyu

---
+++ CodeWars/8kyu tasks/validateCodeWithSimpleRegex.js
@@ -0,0 +1,16 @@
+/*
+  Title:
+    validate code with simple regex
+
+  Description:
+    Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+    You can assume the input will always be a number.
+
+  Kata Link:
+    https://www.codewars.com/kata/56a25ba95df27b7743000016/train/javascript
+*/
+
+function validateCode(code) {
+  return /^(1|2|3)/.test(code);
+}
