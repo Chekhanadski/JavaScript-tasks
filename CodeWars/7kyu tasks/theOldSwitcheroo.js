a1167bca27d16294f0f083f348f6d468a59982c8
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-09-02 19:43:40 +0200

    solved task "The old switcheroo" 7kyu

---
+++ CodeWars/7kyu tasks/theOldSwitcheroo.js
@@ -0,0 +1,25 @@
+/*
+  Title:
+    The old switcheroo
+
+  Description:
+    Write a function
+
+      vowel2index(str)
+
+    that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string.
+    
+    E.g:
+      vowel2index('this is my string') == 'th3s 6s my str15ng'
+      vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+      vowel2index('') == ''
+
+    Your function should be case insensitive to the vowels.
+
+  Kata Link:
+    https://www.codewars.com/kata/55d410c492e6ed767000004f/train/javascript
+*/
+
+function vowel2index(str) {
+  return str.replace(/[a,e,i,o,u]/gi, (match, offset) => offset + 1);
+}
