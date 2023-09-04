9f6bee0e181840eafa11d1f0a83833eb353cf45a
Chekhanadski Andrei <chekhanadski@gmail.com>
2023-09-04 22:06:47 +0200

    solved task"Return Two Highest Values in List"8kyu

---
+++ CodeWars/8kyu tasks/returnTwoHighestValuesInList.js
@@ -0,0 +1,23 @@
+/*
+  Title:
+    Return Two Highest Values in List
+
+  Description:
+    In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+    The result should also be ordered from highest to lowest.
+
+    Examples:
+
+      [4, 10, 10, 9]  =>  [10, 9]
+      [1, 1, 1]  =>  [1]
+      []  =>  []  
+
+  Kata Link:
+    https://www.codewars.com/kata/57ab3c09bb994429df000a4a/train/javascript
+*/
+
+function twoHighest(arr) {
+  const uniqueSortedArr = [...new Set(arr)].sort((a, b) => b - a);
+  return uniqueSortedArr.slice(0, 2);
+}
